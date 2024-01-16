52e138c112434c46f2afa41821fd9e6dc86bd8d4
fenrirsoftware <fenrirsoftware@protonmail.com>
2024-01-16 15:46:47 +0300

    Dynamic Form Design and Multiple Copy

--- clipboard/Form1.cs
+++ clipboard/Form1.cs
@@ -11,6 +11,13 @@ namespace clipboard
         // DLL'yi çağırmak için P/Invoke kullanılır.
         [DllImport("CopyPasteRelease.dll", CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr ClipBoardData();
+      
+
+
+       
+
+
+
 
         private bool dragging = false;
 
@@ -30,26 +37,59 @@ namespace clipboard
         private async void Form1_Load(object sender, EventArgs e)
         {
             await LoadAsyncData();
+           
+            AddScrollBar();
         }
         //hello world helmsys kruvazör
         private async Task LoadAsyncData()
         {
             while (keepRunning)
             {
-                // Asenkron olarak testFonksiyon'u çağır
                 IntPtr result = await Task.Run(() => ClipBoardData());
-
-                // IntPtr'i C# stringine dönüştür
                 string message = Marshal.PtrToStringAnsi(result);
 
-                // Sonucu ekrana yazdır
-                textBox1.Text = message;
+                // Eğer gelen veri bir önceki veri ile eşit değilse
+                if (message != previousData)
+                {
+                    CreateAndFillTextBox(message);
+                    previousData = message;
+                }
 
-                // Bir süre bekleyebilirsiniz, eğer gerekiyorsa
-                await Task.Delay(1000); // Örneğin, 1 saniye bekleyin
+                await Task.Delay(1000);
             }
         }
+        private string previousData = string.Empty;
+        private int textBoxCount = 0;
+        private void CreateAndFillTextBox(string text)
+        {
+            TextBox newTextBox = new TextBox();
+            newTextBox.Size = new Size(290, 53);
+            newTextBox.Location = new Point(5, 5 + textBoxCount * 60);
+            newTextBox.Multiline = true;
+            newTextBox.ScrollBars = ScrollBars.Vertical;
+            newTextBox.Text = text;
+            Controls.Add(newTextBox);
+            textBoxCount++;
+        }
 
+        private void AddScrollBar()
+        {
+            VScrollBar vScrollBar = new VScrollBar();
+            vScrollBar.Dock = DockStyle.Right;
+            vScrollBar.Scroll += VScrollBar_Scroll;
+            Controls.Add(vScrollBar);
+        }
+
+        private void VScrollBar_Scroll(object sender, ScrollEventArgs e)
+        {
+            foreach (Control control in Controls)
+            {
+                if (control is TextBox)
+                {
+                    control.Top -= e.NewValue - e.OldValue;
+                }
+            }
+        }
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             // Form kapatıldığında döngüyü sonlandır
@@ -60,7 +100,7 @@ namespace clipboard
         {
             if (e.Button == MouseButtons.Left)
             {
-                dragging = true;
+                dragging = true;    
                 dragStartPoint = new Point(e.X, e.Y);
             }
         }
