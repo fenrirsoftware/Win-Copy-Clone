4cf1621f57d9fb48585fe16fbc8161ee5b835e11
fenrirsoftware <fenrirsoftware@protonmail.com>
2024-01-18 17:12:31 +0300

    glassmorphism Form Design and New Features

--- clipboard/Form1.Designer.cs
+++ clipboard/Form1.Designer.cs
@@ -28,28 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-4, 360);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(403, 44);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.ClientSize = new System.Drawing.Size(296, 450);
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(392, 401);
+            this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.TopMost = true;
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
-            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
-            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- clipboard/Form1.cs
+++ clipboard/Form1.cs
@@ -1,118 +1,116 @@
 ﻿using System;
 using System.Drawing;
 using System.Runtime.InteropServices;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WinBlur;
+using static WinBlur.UI;
 
 namespace clipboard
 {
     public partial class Form1 : Form
     {
-        // DLL'yi çağırmak için P/Invoke kullanılır.
         [DllImport("CopyPasteRelease.dll", CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr ClipBoardData();
-      
-
-
-       
-
 
 
 
         private bool dragging = false;
-
         private Point dragStartPoint;
-
-
         private bool keepRunning = true;
+
 
         public Form1()
         {
             InitializeComponent();
-
-            // Form kapatıldığında olayları dinle
-            FormClosing += Form1_FormClosing;
+            FormBorderStyle = FormBorderStyle.None;
+            DoubleBuffered = true;
         }
 
         private async void Form1_Load(object sender, EventArgs e)
         {
+            InitializeComponent();
+            FormBorderStyle = FormBorderStyle.None;
+            DoubleBuffered = true;
+
+            SetBlurStyle(cntrl: this, blurType: BlurType.Acrylic, designMode: Mode.DarkMode);
+
+            // Apply rounded corners to the form
+         
+          
+
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            flowLayoutPanel1.Size = new Size(410, 420);
+            flowLayoutPanel1.AutoScroll = true;
+
+            Controls.Add(flowLayoutPanel1);
             await LoadAsyncData();
-           
-            AddScrollBar();
         }
-        //hello world helmsys kruvazör
+
+
+
         private async Task LoadAsyncData()
         {
             while (keepRunning)
             {
+                await Task.Delay(100);
                 IntPtr result = await Task.Run(() => ClipBoardData());
                 string message = Marshal.PtrToStringAnsi(result);
 
-                // Eğer gelen veri bir önceki veri ile eşit değilse
                 if (message != previousData)
                 {
                     CreateAndFillTextBox(message);
                     previousData = message;
                 }
-
-                await Task.Delay(1000);
             }
         }
+
         private string previousData = string.Empty;
         private int textBoxCount = 0;
+        private FlowLayoutPanel flowLayoutPanel1;
 
-           
-        private void CreateAndFillTextBox(string text)
+       private void CreateAndFillTextBox(string text)
+{
+    Button newTextBox = new Button();
+    newTextBox.Size = new Size(389, 70);
+    newTextBox.FlatStyle = FlatStyle.Flat;
+    newTextBox.FlatAppearance.BorderSize = 0;
+    
+    // Add a margin of 10 pixels from the top
+    newTextBox.Location = new Point(5, 5 + textBoxCount * 90); // Adjusted the margin and vertical spacing
+    
+    newTextBox.TextAlign = ContentAlignment.MiddleLeft;
+
+    newTextBox.Text = text;
+    newTextBox.Font = new Font("Arial", 9, FontStyle.Regular);
+    newTextBox.ForeColor = Color.FromArgb(220, 220, 220);
+    newTextBox.BackColor = Color.FromArgb(60, 60, 60);
+
+    // Apply rounded corners to the button
+    newTextBox.Click += Button_Click;
+
+    Controls.Add(newTextBox);
+    textBoxCount++;
+    flowLayoutPanel1.Controls.Add(newTextBox);
+}
+
+
+        private void Button_Click(object sender, EventArgs e)
         {
-            TextBox newTextBox = new TextBox();
-            newTextBox.Size = new Size(290, 53);
-            newTextBox.Location = new Point(5, 5 + textBoxCount * 60);
-            newTextBox.Multiline = true;
-            newTextBox.ReadOnly = true;
-           // newTextBox.Enabled = false;
-
-            newTextBox.ScrollBars = ScrollBars.Vertical;
-            newTextBox.Text = text;
-            Controls.Add(newTextBox);
-            textBoxCount++;
+            // TODO: Butona tıklama işlemleri
         }
 
-        private void AddScrollBar()
-        {
-            VScrollBar vScrollBar = new VScrollBar();
-            vScrollBar.Dock = DockStyle.Right;
-            
-            vScrollBar.Scroll += VScrollBar_Scroll;
-            Controls.Add(vScrollBar);
-
-        }
-
-        private void VScrollBar_Scroll(object sender, ScrollEventArgs e)
-        {
-            foreach (Control control in Controls)
-            {
-                if (control is TextBox)
-                {
-                    control.Top -= e.NewValue - e.OldValue;
-                }
-            }
-        }
-        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            // Form kapatıldığında döngüyü sonlandır
-            keepRunning = false;
-        }
-
-        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
         {
             if (e.Button == MouseButtons.Left)
             {
-                dragging = true;    
+                dragging = true;
                 dragStartPoint = new Point(e.X, e.Y);
             }
         }
 
-        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
         {
             if (e.Button == MouseButtons.Left)
             {
@@ -120,7 +118,7 @@ namespace clipboard
             }
         }
 
-        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
         {
             if (dragging)
             {
@@ -128,5 +126,21 @@ namespace clipboard
                 Location = new Point(Location.X + dragDelta.X, Location.Y + dragDelta.Y);
             }
         }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            keepRunning = false;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // Button kenarlarını yuvarlamak için özel metot
+    
+
+
+     
     }
 }
